a3fe2100254017eed5511118f2130c567f9e6731
pedrodel26 <pedromodesto26@gmail.com>
2024-08-13 19:56:58 -0300

    commit 2.0

---
+++ Cinetopia/HomeViewController.swift
@@ -0,0 +1,110 @@
+//
+//  ViewController.swift
+//  Cinetopia
+//
+//  Created by Pedro Delmondes  on 09/08/2024.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    private lazy var logoImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage.logo)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        
+        return imageView
+    }()
+    
+    private lazy var coupleImageView: UIImageView = {
+        let imageView = UIImageView(image: .couple)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        
+        return imageView
+    }()
+    
+    private lazy var welcomeLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "O lugar ideal para buscar, salvar e organizar seus filmes favoritos!"
+        label.textColor = .white
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 20, weight: .semibold)
+        
+        return label
+    }()
+    
+    private lazy var welcomeButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Clique aqui!", for: .normal)
+        button.backgroundColor = .buttonBackground
+        button.setTitleColor(.background, for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 24)
+        button.layer.cornerRadius = 32
+        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [logoImageView, coupleImageView,welcomeLabel, welcomeButton])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.spacing = 46
+        stackView.alignment = .center
+        
+        return stackView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .background
+        addSubviews()
+        setupConstraints()
+    }
+    
+    @objc private func buttonPressed() {
+        navigationController?.pushViewController(MoviesViewController(), animated: true)
+    }
+    
+    private func addSubviews() {
+        view.addSubview(stackView)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -64),
+            
+            welcomeButton.heightAnchor.constraint(equalToConstant: 64),
+            welcomeButton.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 64),
+            welcomeButton.trailingAnchor.constraint(equalTo: stackView.trailingAnchor, constant: -64),
+            
+            /*logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 24),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            coupleImageView.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 32),
+            coupleImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            welcomeLabel.topAnchor.constraint(equalTo: coupleImageView.bottomAnchor, constant: 32),
+            welcomeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            welcomeLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            welcomeButton.topAnchor.constraint(equalTo: welcomeLabel.bottomAnchor, constant: 16),
+            welcomeButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 64),
+            welcomeButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -64),
+            welcomeButton.heightAnchor.constraint(equalToConstant: 64)*/
+            
+        ])
+    }
+}
+
